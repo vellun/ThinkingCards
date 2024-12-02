bf1da241366b228355a1f7757e6bfeff11f04afd
vellun <moonpassiflora@gmail.com>
2024-12-02 21:34:07 +0300

    add navbar

--- justhink_server/justhink/lobby/views.py
+++ justhink_server/justhink/lobby/views.py
@@ -9,6 +9,7 @@ import django.core.cache
 
 class LobbyIdAPIView(views.APIView):
     def get(self, request):
+        
         uid = uuid.uuid4().hex
 
         django.core.cache.cache.set(
@@ -20,16 +21,16 @@ class LobbyIdAPIView(views.APIView):
 
 class LobbyAPIView(views.APIView):
     def get(self, request, uid):
-        users = django.core.cache.cache.get(f"lobby-{uid}")
+        lobby_users = django.core.cache.cache.get(f"lobby-{uid}")
 
-        if not users:
+        if not lobby_users:
             raise exceptions.NotFound(
                 detail="Lobby does not exist"
             )
 
         cur_user = request.user.username
 
-        if cur_user not in users:
+        if cur_user not in lobby_users:
             channel_layer = channels.layers.get_channel_layer()
             asgiref.sync.async_to_sync(channel_layer.group_send)(
                 f"lobby_{uid}",
@@ -39,9 +40,9 @@ class LobbyAPIView(views.APIView):
                 },
             )
 
-        if not users.get(cur_user, None):
-            users[cur_user] = "player"
+        if not lobby_users.get(cur_user, None):
+            lobby_users[cur_user] = "player"
 
-            django.core.cache.cache.set(f"lobby-{uid}", users)
+            django.core.cache.cache.set(f"lobby-{uid}", lobby_users)
 
-        return response.Response([users.keys()])
+        return response.Response([lobby_users.keys()])
